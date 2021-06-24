1d5d8c61fdc84e8163c8ac6133d25472e472a98e
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-06-24 10:02:09 -0700

    Add flavor

--- demo/src/main/java/in/juspay/myapplication/MainActivity.java
+++ demo/src/main/java/in/juspay/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     TextView tv_hello =findViewById(R.id.tv_hello);
-    tv_hello.setText(HelloUtil.getMessage());
+    tv_hello.setText(HelloUtil.getMessage()+":"+BuildConfig.FLAVOR);
   }
 }
